ce50cee8bbecb44886d3877f749ce9ed8dfd5620
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-07 00:53:00 +0300

    Add CartView and ProductCart components: implement shopping cart functionality

---
+++ src/views/CartView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue'
+const products = ref([
+  { name: 'Product 1', price: 10 },
+  { name: 'Product 2', price: 20 },
+  { name: 'Product 3', price: 30 },
+])
+
+const totalPrice = computed(() => {
+  return products.value.reduce((total, product) => total + product.price, 0)
+})
+
+const removeFromCart = (index) => {
+  products.value.splice(index, 1)
+}
+</script>
+<template>
+  <div class="product-cart">
+    <h2>Shopping Cart</h2>
+    <ul>
+      <li v-for="(product, index) in products" :key="index">
+        {{ product.name }} - {{ product.price }}$
+        <button @click="removeFromCart(index)">Remove</button>
+      </li>
+    </ul>
+    <p>Total: {{ totalPrice }}$</p>
+  </div>
+</template>
